889bf8313c9b160cd203ef452f3f69f4d0f7a4e2
Gabriel Proença Alves <gproenca42@gmail.com>
2024-04-28 23:26:31 -0300

    Modify sll function prototypes
    Now, sll functions require list** instead list* (to easily functions writing)

--- sll.h
+++ sll.h
@@ -5,27 +5,27 @@
 typedef struct list_ list;
 
 /* returns a recently created list */
-list*
+list**
 list_new(size_t);
 
 /* frees the specified list */
 void
-list_free(list*);
+list_free(list**);
 
 /* appends a new item to the list */
 int
-list_append(list*, void*);
+list_append(list**, void*);
 
 /* pops the last list item */
-void*
-list_pop(list*);
+int
+list_pop(list**);
 
 /* inserts a new item on specified index of list */
 int
-list_insert(list*, size_t, void*);
+list_set(list**, size_t, void*);
 
 /* deletes the specified index of list */
 int
-list_delete(list*, size_t);
+list_del(list**, size_t);
 
 #endif /* SLL_H */
