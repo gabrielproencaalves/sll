46b6243b3a1c8575d8de9180084b6c4a48df7354
Gabriel Proença Alves <gproenca42@gmail.com>
2024-04-28 23:35:23 -0300

    WIP: Implement list_set function

--- sll.c
+++ sll.c
@@ -1,4 +1,5 @@
 #include <sll.h>
+#include <stdlib.h>
 
 struct list_
 {
@@ -13,30 +14,91 @@ struct list_
 };
 
 /* list structure definition */
-list*
+list**
 list_new(size_t usiz)
 {
   list* tmp = malloc(sizeof(list));
   tmp->usize = usiz;
   tmp->len = 0;
 
-  return tmp;
+  return &tmp;
 }
 
 /* frees the specified list */
 void
-list_free(list* l)
+list_free(list** l)
 {
-  if (l)
+  if (l*)
     {
-      free(l->index);
-      free(l);
+      free((l*)->index);
+      free(l*);
     }
 }
 
 /* appends a new item to the list */
 int
-list_append(list* l, void* v)
+list_append(list** l, void* v)
 {
-  if (l && 
+  if (l* && v)
+    {
+      list* tmp;
+      size_t nsize;
+
+      nsize = (l*)->usize * ((l*)->len + 1);
+      tmp = realloc(l*, nsize);
+
+      if (tmp)
+	{
+	  memcpy((tmp + tmp->usize * tmp->len++), v, tmp->usize);
+
+	  l* = tmp;
+
+	  return 1;
+	}
+    }
+
+  return 0;
+}
+
+/* pops the last list item */
+int
+list_pop(list** l)
+{
+  if (l*)
+    {
+      list* tmp;
+      size_t nsize;
+
+      nsize = (l*)->usize * ((l*)->len - 1);
+      tmp = realloc(l*, nsize);
+
+      if (tmp)
+	{
+	  l* = tmp;
+	  (l*)->len--;
+
+	  return 1;
+	}
+    }
+
+  return 0;
+}
+
+/* inserts a new value on specified index of list */
+int
+list_set(list** l, size_t i, void* v)
+{
+  if (l* && i > 0)
+    {
+      list* tmp;
+      size_t nsize;
+
+      nsize = (l*)->usize * ((l*)->len + 1);
+      tmp = malloc(nsize);
+
+      if (tmp)
+        {
+
+        }
+    }
 }
